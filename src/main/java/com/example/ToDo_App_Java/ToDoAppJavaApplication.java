e21083ae31511810941412b4ccfc090f86613c72
root <root@IMP-LAP-0544.impelsys.com>
2025-05-07 15:25:26 +0530

    Initial Commit

---
+++ src/main/java/com/example/ToDo_App_Java/ToDoAppJavaApplication.java
@@ -0,0 +1,15 @@
+package com.example.ToDo_App_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ToDoAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppJavaApplication.class, args);
+		System.out.println("Hello Prashant");
+	}
+
+}
---
+++ src/test/java/com/example/ToDo_App_Java/ToDoAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToDo_App_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
